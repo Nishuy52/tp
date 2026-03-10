a3ce8a2340716ddb25a5fbdc55d5b884b7cf4e61
kame no oshiri <chng.sean2@gmail.com>
2026-03-10 11:49:21 +0800

    feat(Income): add Income subclass to Transaction

---
+++ src/main/java/seedu/duke/Income.java
@@ -0,0 +1,28 @@
+package seedu.duke;
+
+import java.time.LocalDate;
+
+public class Income extends Transaction {
+    public Income(String category, double amount, String description, LocalDate date) {
+        super(category, amount, description, date);
+    }
+
+    public Income(String category, double amount, String description) {
+        super(category, amount, description);
+    }
+
+    public Income(String category, double amount) {
+        super(category, amount);
+    }
+
+    @Override
+    public String getType() {
+        return "Income";
+    }
+
+    @Override
+    public String toString() {
+        String descriptionSuffix = description.isEmpty() ? "" : " \"" + description + "\"";
+        return String.format("[Income] %s%s $%.2f (%s)", category, descriptionSuffix, amount, date);
+    }
+}
